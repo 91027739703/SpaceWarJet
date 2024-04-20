88810268082609b7e09bc69b2c653ba4e109aa47
حسین اخلاقی <146563551+91027739703@users.noreply.github.com>
2024-04-20 20:54:19 +0330

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace jangfazaee
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            bullet2 = new PictureBox();
+            plane2 = new PictureBox();
+            plane3 = new PictureBox();
+            plane1 = new PictureBox();
+            bullet1 = new PictureBox();
+            bullet3 = new PictureBox();
+            label3 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            twoumber = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)bullet2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)plane2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)plane3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)plane1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bullet1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bullet3).BeginInit();
+            SuspendLayout();
+            // 
+            // bullet2
+            // 
+            bullet2.BackColor = Color.Transparent;
+            bullet2.Image = (Image)resources.GetObject("bullet2.Image");
+            bullet2.Location = new Point(582, 243);
+            bullet2.Name = "bullet2";
+            bullet2.Size = new Size(24, 150);
+            bullet2.SizeMode = PictureBoxSizeMode.AutoSize;
+            bullet2.TabIndex = 18;
+            bullet2.TabStop = false;
+            bullet2.Click += bullet2_Click;
+            // 
+            // plane2
+            // 
+            plane2.BackColor = Color.Transparent;
+            plane2.Image = (Image)resources.GetObject("plane2.Image");
+            plane2.Location = new Point(551, 80);
+            plane2.Name = "plane2";
+            plane2.Size = new Size(69, 80);
+            plane2.SizeMode = PictureBoxSizeMode.AutoSize;
+            plane2.TabIndex = 17;
+            plane2.TabStop = false;
+            plane2.Click += plane2_Click;
+            // 
+            // plane3
+            // 
+            plane3.BackColor = Color.Transparent;
+            plane3.Image = Properties.Resources.space_jet11;
+            plane3.Location = new Point(672, 80);
+            plane3.Name = "plane3";
+            plane3.Size = new Size(58, 80);
+            plane3.SizeMode = PictureBoxSizeMode.AutoSize;
+            plane3.TabIndex = 16;
+            plane3.TabStop = false;
+            plane3.Click += plane3_Click;
+            // 
+            // plane1
+            // 
+            plane1.BackColor = Color.Transparent;
+            plane1.Image = Properties.Resources.jet21;
+            plane1.Location = new Point(439, 80);
+            plane1.Name = "plane1";
+            plane1.Size = new Size(60, 80);
+            plane1.SizeMode = PictureBoxSizeMode.AutoSize;
+            plane1.TabIndex = 15;
+            plane1.TabStop = false;
+            plane1.Click += plane1_Click;
+            // 
+            // bullet1
+            // 
+            bullet1.BackColor = Color.Transparent;
+            bullet1.Image = (Image)resources.GetObject("bullet1.Image");
+            bullet1.Location = new Point(473, 243);
+            bullet1.Name = "bullet1";
+            bullet1.Size = new Size(24, 150);
+            bullet1.SizeMode = PictureBoxSizeMode.AutoSize;
+            bullet1.TabIndex = 14;
+            bullet1.TabStop = false;
+            bullet1.Click += bullet1_Click;
+            // 
+            // bullet3
+            // 
+            bullet3.BackColor = Color.Transparent;
+            bullet3.Image = Properties.Resources.bullet33;
+            bullet3.Location = new Point(689, 243);
+            bullet3.Name = "bullet3";
+            bullet3.Size = new Size(24, 150);
+            bullet3.SizeMode = PictureBoxSizeMode.AutoSize;
+            bullet3.TabIndex = 13;
+            bullet3.TabStop = false;
+            bullet3.Click += bullet3_Click;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Book Antiqua", 20.25F);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(12, 192);
+            label3.Name = "label3";
+            label3.Size = new Size(516, 48);
+            label3.TabIndex = 12;
+            label3.Text = "Choose the bullet you want :(player)";
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Book Antiqua", 20.25F);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(2, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(526, 45);
+            label2.TabIndex = 11;
+            label2.Text = "Choose your desired plane :(player)";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Bookman Old Style", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(672, 511);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 41);
+            button1.TabIndex = 19;
+            button1.Text = "NEXT ->>";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // twoumber
+            // 
+            twoumber.BackColor = Color.White;
+            twoumber.FlatStyle = FlatStyle.Flat;
+            twoumber.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            twoumber.ForeColor = Color.Black;
+            twoumber.Location = new Point(564, 511);
+            twoumber.Name = "twoumber";
+            twoumber.Size = new Size(102, 41);
+            twoumber.TabIndex = 20;
+            twoumber.Text = "DUEL";
+            twoumber.UseVisualStyleBackColor = false;
+            twoumber.Click += twoumber_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Book Antiqua", 20.25F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(2, 377);
+            label1.Name = "label1";
+            label1.Size = new Size(365, 48);
+            label1.TabIndex = 12;
+            label1.Text = "Choose the game mode :";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Book Antiqua", 14.25F);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(328, 428);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 55);
+            button2.TabIndex = 21;
+            button2.Text = " EASY";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Violet;
+            button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Book Antiqua", 14.25F);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(487, 429);
+            button3.Name = "button3";
+            button3.Size = new Size(144, 55);
+            button3.TabIndex = 21;
+            button3.Text = "MEDIUM";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackgroundImage = (Image)resources.GetObject("button4.BackgroundImage");
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Book Antiqua", 14.25F);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(651, 428);
+            button4.Name = "button4";
+            button4.Size = new Size(144, 55);
+            button4.TabIndex = 21;
+            button4.Text = "IMPOSSIBLE";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.pngtree_3d_planets_in_space_wallpapers_for_mobile_picture_image_2670749;
+            ClientSize = new Size(800, 555);
+            ControlBox = false;
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(twoumber);
+            Controls.Add(button1);
+            Controls.Add(bullet2);
+            Controls.Add(plane2);
+            Controls.Add(plane3);
+            Controls.Add(plane1);
+            Controls.Add(bullet1);
+            Controls.Add(bullet3);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Name = "Form2";
+            StartPosition = FormStartPosition.Manual;
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)bullet2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)plane2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)plane3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)plane1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bullet1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bullet3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox bullet2;
+        private PictureBox plane2;
+        private PictureBox plane3;
+        private PictureBox plane1;
+        private PictureBox bullet1;
+        private PictureBox bullet3;
+        private Label label3;
+        private Label label2;
+        private Button button1;
+        private Button twoumber;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
